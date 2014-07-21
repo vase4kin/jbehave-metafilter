c24e3689206db81913b7075f1b79aa92fb2e85d3
Andrey Tolpeev <atolpeev@crystals.ru>
2014-07-21 14:31:02 +0400

    Initial commit

---
+++ src/main/java/jbehave/metafilter/steps/EndUserSteps.java
@@ -0,0 +1,40 @@
+package jbehave.metafilter.steps;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.steps.ScenarioSteps;
+import java.util.List;
+import static org.fest.assertions.Assertions.assertThat;
+
+import jbehave.metafilter.pages.DictionaryPage;
+
+public class EndUserSteps extends ScenarioSteps {
+
+    DictionaryPage dictionaryPage;
+
+    @Step
+    public void enters(String keyword) {
+        dictionaryPage.enter_keywords(keyword);
+    }
+
+    @Step
+    public void starts_search() {
+        dictionaryPage.lookup_terms();
+    }
+
+    @Step
+    public void should_see_definition(String definition) {
+        assertThat(dictionaryPage.getDefinitions()).contains(definition);
+    }
+
+    @Step
+    public void is_the_home_page() {
+        dictionaryPage.open();
+    }
+
+    @Step
+    public void looks_for(String term) {
+        enters(term);
+        starts_search();
+    }
+}
---
+++ src/test/java/jbehave/metafilter/jbehave/AcceptanceTestSuite.java
@@ -0,0 +1,5 @@
+package jbehave.metafilter.jbehave;
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+
+public class AcceptanceTestSuite extends ThucydidesJUnitStories {}
